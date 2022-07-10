7aa76e408d4605ec0d6373852d1c590f8cac949b
itsheng <1624708355@qq.com>
2022-07-10 13:35:22 +0800

    [提交类型]：代码 [修改版本]： [修改单编号]： [缺陷编号]： [修改内容说明]：

--- leetcode/src/com/itsheng/leetcode/twosum/twoSumn.java
+++ leetcode/src/com/itsheng/leetcode/twosum/twoSumn.java
@@ -27,6 +27,8 @@ public class twoSumn {
         int target = 9;
         int[] ints = twoSum(ans, target);
         System.out.println(Arrays.toString(ints));
+        int[] int1s = twoSum1(ans, target);
+        System.out.println(Arrays.toString(int1s));
 
     }
     /**
@@ -51,6 +53,22 @@ public class twoSumn {
                 ans[1] = i;
             }
             map.put(nums[i],i);
+        }
+        return ans;
+    }
+    // 暴力破解
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] ans = new int[2];
+        if (null == nums || nums.length == 0) {
+            return new int[0];
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
         }
         return ans;
     }
