fa58008c2b6b07703ea1c054fa133b92359e4c8c
itsheng <1624708355@qq.com>
2022-07-10 12:21:21 +0800

    [提交类型]：需求/缺陷/代码整理/代码重构/单元测试用例补充 [修改版本]： [修改单编号]： [缺陷编号]： [修改内容说明]：

---
+++ leetcode/src/com/itsheng/leetcode/OneDayLeetcode.java
@@ -0,0 +1,13 @@
+package com.itsheng.leetcode;
+/**
+ * @Author
+ * @Description //TODO
+ * @Date 12:15 2022/7/10
+ * @Param
+ * @return
+**/
+public class OneDayLeetcode {
+    public static void main(String[] args) {
+        System.out.println("helloWord");
+    }
+}
